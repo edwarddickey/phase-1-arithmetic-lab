97124a0c359862cfd8527103fc9a1ec6bea1c03f
Edward Dickey <edward.dickey@ECDs-MacBook-Pro.local>
2022-08-08 17:55:49 -0600

    commit tree 64265789329087caf8fbb2664d9131ecf17158b8

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
+let num1 =2
+let num2 =31
+let multiply = num1 *num2;
 // Write your code here
+function multiply(num1,num2){
+    
+    
+    return(num1*num2);
+}
+multiply(2,31);
+function random(){
+    let rand = parseInt(Math.random()*10);
+}
+random();
+//const random = Math.ceil(Math.random());
